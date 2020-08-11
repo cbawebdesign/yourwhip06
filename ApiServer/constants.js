3715386ac32381aaee4c3981ce47f962802ce271
Henk Corporaal <henk@freed.nl>
2020-08-11 14:51:47 +0200

    small fix

--- ApiServer/constants.js
+++ ApiServer/constants.js
@@ -2,17 +2,38 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const development = true;
+
+const MONGODB_CONNECTION_STRING = '';
+
+const SENDGRID_USERNAME = '';
+const SENDGRID_PASSWORD = '';
+
+const CLOUDINARY_CLOUD_NAME = '';
+const CLOUDINARY_API_KEY = '';
+const CLOUDINARY_API_SECRET = '';
+const CLOUDINARY_MEDIA_FOLDER = '';
+
 exports.MONGODB = {
-  connectionString: process.env.MONGODB_CONNECTION,
+  connectionString: development
+    ? MONGODB_CONNECTION_STRING
+    : process.env.MONGODB_CONNECTION,
 };
 
 exports.SENDGRID = {
-  username: process.env.SENDGRID_USERNAME,
-  password: process.env.SENDGRID_PASSWORD,
+  username: development ? SENDGRID_USERNAME : process.env.SENDGRID_USERNAME,
+  password: development ? SENDGRID_PASSWORD : process.env.SENDGRID_PASSWORD,
 };
 
 exports.CLOUDINARY = {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: development
+    ? CLOUDINARY_CLOUD_NAME
+    : process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: development ? CLOUDINARY_API_KEY : process.env.CLOUDINARY_API_KEY,
+  api_secret: development
+    ? CLOUDINARY_API_SECRET
+    : process.env.CLOUDINARY_API_SECRET,
+  MEDIA_FOLDER: development
+    ? CLOUDINARY_MEDIA_FOLDER
+    : process.env.CLOUDINARY_MEDIA_FOLDER,
 };
